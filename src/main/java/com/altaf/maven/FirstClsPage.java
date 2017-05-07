de3a22f1b61482e07ed578a6c48b6ae595362e80
Altaf Raheem <araheem@spider-india.local>
2017-05-07 20:47:03 +0530

    First Commit

---
+++ src/main/java/com/altaf/maven/FirstClsPage.java
@@ -0,0 +1,5 @@
+package com.altaf.maven;
+
+public class FirstClsPage {
+
+}
---
+++ src/test/java/com/altaf/maven/FirstClsPageTest.java
@@ -0,0 +1,12 @@
+package com.altaf.maven;
+
+import org.testng.annotations.Test;
+
+public class FirstClsPageTest {
+	
+	//@Test
+	public void func(){
+		System.out.println("In func");
+	}
+
+}
